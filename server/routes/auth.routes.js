d6ee8a15e40cc8055e6b4ed861403f5d1c6b89dd
STiGMA <oFFline13>
2020-12-01 22:04:50 +0300

    JWT Token, Login REST API

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -1,5 +1,7 @@
 const Router = require('express')
 const bcrypt = require('bcrypt')
+const jwt = require("jsonwebtoken")
+const config = require("config")
 const {check, validationResult} = require('express-validator')
 const User = require('../models/User') //импортируем модель пользователя
 const router = new Router() //создаем новый объект роутер
@@ -34,6 +36,42 @@ router.post('/registration',
         }catch (e) {
             console.log(e)
             res.send({message: 'Server Error'}) //отправляем сообщение если есть серверная ошибка
+        }
+    })
+
+router.post('/login',
+    async (req, res) => {
+        try {
+            const {email, password} = req.body //получаем email и password из тела запроса
+            const user = await User.findOne({email}) //находим пользователя по email
+            if (!user) {
+                return res.status(404).json({message: "User not found"}) //если не найден ошибка
+            }
+            //сравним пароль в запросе с БД
+            //пароль храниться защифрованым, сравниеваем с помощью compareSync
+            const isPassValid = bcrypt.compareSync(password, user.password)
+            if (!isPassValid) {
+                return res.status(400).json({message: "Invalid password"})
+            }
+            //функция sign принимает три параметра:
+            // объект с данными который мы хотим поместить в токен
+            // секретный ключ
+            // время жизни токена
+            const token = jwt.sign({id: user.id}, config.get("secretKey"), {expiresIn: "1h"})
+            //после создания токена нужно вернуть его обратно на клиент, а также некоторые данные пользователя
+            return res.json({
+                token,
+                user: {
+                    id: user.id,
+                    email: user.email,
+                    diskSpace: user.diskSpace,
+                    usedSpace: user.usedSpace,
+                    avatar: user.avatar
+                }
+            })
+        } catch (e) {
+            console.log(e)
+            res.send({message: "Server error"})
         }
     })
 
